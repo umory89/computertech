ab9a8bb27db59e7526e8a9d3a2d43f4c7fce14aa
Egitva <9026479948e@gmail.com>
2026-06-16 21:07:20 +0500

    push

---
+++ курсовая2511/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace курсовая2511.Models
+{
+    internal class Employee
+    {
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ курсовая2511/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace курсовая2511.Models
+{
+    internal class User
+    {
+        public Guid id { get; set; }
+        public string Username { get; set; }
+
+        public string passwordHash { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
