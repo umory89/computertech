758a5ab1b830d2c8b897838f3c8d26e4ba7efaac
Egitva <9026479948e@gmail.com>
2026-06-16 19:33:14 +0500

    Добавьте файлы проекта.

---
+++ курсовая2511/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace курсовая2511
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
