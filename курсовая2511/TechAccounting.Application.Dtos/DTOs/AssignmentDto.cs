2daefd328260150e4105bd7dc9a4c014c0a70edf
Egitva <9026479948e@gmail.com>
2026-06-23 21:17:07 +0500

    написал один dto файл

--- курсовая2511/TechAccounting.Application.Dtos/DTOs/AssignmentDto.cs
+++ курсовая2511/TechAccounting.Application.Dtos/DTOs/AssignmentDto.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace курсовая2511.TechAccounting.Application.DTOs
 {
-    internal class AssignmentDto
+    public class AssignmentDto
     {
+        public Guid Id { get; set; }
+
+        public DateTime AssignedDate { get; set; }
+
+        public DateTime? ReturnDate { get; set; }
+
+
+        public string ConditionAtIssue { get; set; } = string.Empty;
+
+        public string ConditionAtReturn { get; set; } = string.Empty;
+
+        public string Notes { get; set; } = string.Empty;
+
+      
+        public Guid EmployeeId { get; set; }
+
+
+        public Guid EquipmentId { get; set; }
     }
 }
